93fbd49c92f080ae71f02033c41d561e66d2c92b
ThomasN72 <thomas.ng@teampay.co>
2022-10-27 16:49:31 -0700

    add everything

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,29 @@
 module.exports = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
+  // stories: [
+  //   "../stories/**/*.stories.mdx",
+  //   "../stories/**/*.stories.@(js|jsx|ts|tsx)",
+  // ],
+  stories: ["../**/stories.@(jsx|mdx|tsx)", "../**/*.stories.@(jsx|mdx|tsx)"],
+  // addons: [
+  //   "@storybook/addon-links",
+  //   "@storybook/addon-essentials",
+  //   "@storybook/addon-interactions",
+  // ],
   addons: [
-    "@storybook/addon-links",
+    "@storybook/addon-a11y",
+    "@storybook/addon-docs",
     "@storybook/addon-essentials",
+    "@storybook/addon-links",
     "@storybook/addon-interactions",
   ],
   framework: "@storybook/react",
-  webpackFinal: async (config, { configType }) => {
-    // Make whatever fine-grained changes you need
-    // Return the altered config
-    config.module.rules.push({
-      test: /\.styl$/,
-      loader: "style-loader!css-loader!stylus-loader",
-    });
-    return config;
-  },
+  // webpackFinal: async (config, { configType }) => {
+  //   // Make whatever fine-grained changes you need
+  //   // Return the altered config
+  //   config.module.rules.push({
+  //     test: /\.styl$/,
+  //     loader: "style-loader!css-loader!stylus-loader",
+  //   });
+  //   return config;
+  // },
 };
